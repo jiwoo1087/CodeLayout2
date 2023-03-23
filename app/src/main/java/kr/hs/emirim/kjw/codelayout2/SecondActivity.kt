6ccfbb576cb17ebf0725f731444b9c431c958daf
jiwoo1087 <kingjiwoo1087@gmail.com>
2023-03-23 20:10:03 +0900

    직접풀어기보기 2-3

---
+++ app/src/main/java/kr/hs/emirim/kjw/codelayout2/SecondActivity.kt
@@ -0,0 +1,11 @@
+package kr.hs.emirim.kjw.codelayout2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
