201cf6b7821047ae38f4cfc6bc6f572204208168
jiwoo1087 <kingjiwoo1087@gmail.com>
2023-03-23 19:51:39 +0900

    Button Event Programming

--- app/src/main/java/kr/hs/emirim/kjw/codelayout2/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/kjw/codelayout2/MainActivity.kt
@@ -2,10 +2,19 @@ package kr.hs.emirim.kjw.codelayout2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btn1 : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn1 = findViewById<Button>(R.id.btn1)
+
+        btn1.setOnClickListener {
+            Toast.makeText(applicationContext, "버튼을 눌렀어요~", Toast.LENGTH_SHORT).show()
+        }
     }
 }
